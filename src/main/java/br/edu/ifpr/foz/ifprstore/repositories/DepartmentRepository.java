49faca502a98a3ae409ec0ff13bdb5047e7453b9
jefferson-chaves <jefferson.chaves@ifpr.edu.br>
2024-09-02 16:07:08 -0300

    feat: add sellers create and delete

--- src/main/java/br/edu/ifpr/foz/ifprstore/repositories/DepartmentRepository.java
+++ src/main/java/br/edu/ifpr/foz/ifprstore/repositories/DepartmentRepository.java
@@ -3,9 +3,11 @@ package br.edu.ifpr.foz.ifprstore.repositories;
 import br.edu.ifpr.foz.ifprstore.connection.ConnectionFactory;
 import br.edu.ifpr.foz.ifprstore.exceptions.DatabaseException;
 import br.edu.ifpr.foz.ifprstore.exceptions.DatabaseIntegrityException;
+import br.edu.ifpr.foz.ifprstore.models.Department;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DepartmentRepository {
 
@@ -13,6 +15,36 @@ public class DepartmentRepository {
 
     public DepartmentRepository(){
         connection = ConnectionFactory.getConnection();
+    }
+
+
+    public List<Department> getAll() {
+
+        List<Department> departments = new ArrayList<>();
+
+        String sql = "SELECT * FROM department";
+
+        try {
+
+            Statement statement = connection.createStatement();
+
+            ResultSet result = statement.executeQuery(sql);
+
+            while (result.next()) {
+
+                Department department = new Department();
+                department.setId(result.getInt("Id"));
+                department.setName(result.getString("Name"));
+                departments.add(department);
+            }
+
+
+        }catch (SQLException e){
+            throw new DatabaseException(e.getMessage());
+        }
+
+        return departments;
+
     }
 
     public void delete(Integer id){
@@ -35,4 +67,6 @@ public class DepartmentRepository {
         }
 
     }
+
+
 }
